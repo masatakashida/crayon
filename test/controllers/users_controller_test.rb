656e7f8e6ac5efb112e09786f9fca2853b99c279
masataka <ar.tec.masa@gmail.com>
2017-08-20 00:53:58 +0900

    テストコメント追記

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+# ----------------------------
+# users#new ページに入れるかテスト
+# ----------------------------
   test "should get new" do
     get users_new_url
     assert_response :success
