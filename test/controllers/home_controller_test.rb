b9a4b6fd3118440dcdc21492e634ffa17d191600
masataka <ar.tec.masa@gmail.com>
2017-08-20 00:29:51 +0900

    コメント追記

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,11 +1,16 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  # -----------------------
+  # home#topが表示される	
+  # -----------------------
   test "should get top" do
     get home_top_url
     assert_response :success
   end
-
+  # -----------------------
+  # home#aboutが表示される
+  # -----------------------
   test "should get about" do
     get home_about_url
     assert_response :success
