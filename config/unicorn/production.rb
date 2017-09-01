f31665c09590a81b9a899fb0f55df7d177e4bdb9
masataka <ar.tec.masa@gmail.com>
2017-09-01 22:40:15 +0900

    unicorn

---
+++ config/unicorn/production.rb
@@ -0,0 +1,16 @@
+WORKING_DIR = "/home/deploy/crayon"
+
+worker_processes 2 # ワーカプロセス数
+working_directory "#{WORKING_DIR}"
+
+#listen "/var/run/nginx-rails.sock" # ソケットファイル
+#listen 8080
+listen "#{WORKING_DIR}/tmp/sockets/unicorn.sock"
+
+pid    "tmp/pids/unicorn.pid"  # pidファイル
+
+# 標準出力、標準エラー出力先
+stdout_path "log/unicorn.stdout.log"
+stderr_path "log/unicorn.stderr.log"
+
+
